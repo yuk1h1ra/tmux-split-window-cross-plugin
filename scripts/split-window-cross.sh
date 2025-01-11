bb9dd31c75efceb4c742e50115fe5433f3ba45c8
yuk1h1ra <iam.yuk1h1ra.me@gmail.com>
2025-01-11 12:07:46 +0900

    Fix script

--- scripts/split-window-cross.sh
+++ scripts/split-window-cross.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# 1. Split horizontally
-tmux split-window -h
-
-# 2. Split vertically
+# 1. Split vertically
 tmux split-window -v
+
+# 2. Split horizontally
+tmux split-window -h
 
 # 3. Move up one pane
 tmux select-pane -U
 
-# 4. Split vertically
-tmux split-window -v
+# 4. Split horizontally
+tmux split-window -h
 
 # 5. Move one pane to the left
 tmux select-pane -L
